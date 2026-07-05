efb85c54436502b0f37120e63af036ef8afad25a
Imas7264 <samiansariwork@gmail.com>
2026-07-05 21:48:10 +0530

    Solved leetcode problem 274

--- Leetcode/274 H-Index.java
+++ Leetcode/274 H-Index.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 public class Test3
 {
  public static void main(String[] args)
@@ -65,22 +63,5 @@ class Solution
 //   }
 
 //   return 0;
-//  }
-// }
-
-
-
-// class Solution
-// {
-//  public int singleNumber(int[] nums)
-//  {
-//   int sum1=0, sum2=0;
-//   for(int i : nums)
-//   {sum1 = sum1^i; sum2 += i;}
-
-//   // for(int i=0; i<nums.length; i++)
-//   // {nums[i] = nums[i]sum; System.out.println(nums[i]);}
-
-//   return sum2-sum1;
 //  }
 // }
