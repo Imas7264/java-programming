75071010f43fc631e7c4792361bb947255868ca5
Imas7264 <samiansariwork@gmail.com>
2026-08-02 15:22:50 +0530

    Solved leetcode 205, 283 and 2095

---
+++ Leetcode/205 Isomorphic Strings.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Test3
+{
+ public static void main(String[] args)
+ {
+  Solution obj = new Solution();
+  int nums[][] = {{1,2,3},{4,5,6},{7,8,9}};
+  System.out.println(obj.isIsomorphic("aa", "ab"));
+ }
+}
+
+
+
+class Solution
+{
+ public boolean isIsomorphic(String s, String t)
+ {
+  if(s.length() != t.length())
+  {return false;}
+
+  HashMap<Character, Character> map1 = new HashMap<>();
+  HashMap<Character, Character> map2 = new HashMap<>();
+  
+  for(int i=0; i<s.length(); i++)
+  {
+   if(map1.containsKey(s.charAt(i)) && map1.get(s.charAt(i)) != t.charAt(i))
+   {return false;}
+
+   if(map2.containsKey(t.charAt(i)) && map2.get(t.charAt(i)) != s.charAt(i))
+   {return false;}
+
+   map1.put(s.charAt(i), t.charAt(i));
+   map2.put(t.charAt(i), s.charAt(i));
+  }
+
+  return true;
+ }
+}
--- Leetcode/724 Find Pivot Index.java
+++ Leetcode/724 Find Pivot Index.java
@@ -30,3 +30,27 @@ class Solution
   return -1;
  }
 }
+
+
+
+// Optimal runtime but non-optimal space complexities, accepted, uses O(n) space but can be solved in O(1)
+// class Solution 
+// {
+//  public int pivotIndex(int[] nums)
+//  {
+//   int preSum[] = new int[nums.length], sum=0, pivot=-1;
+
+//   for(int i=1; i<nums.length; i++)
+//   {preSum[i] = nums[i-1]+preSum[i-1];}
+
+//   for(int i=nums.length-1; i>-1; i--)
+//   {
+//    if(preSum[i] == sum)
+//    {pivot = i;}
+
+//    sum += nums[i];
+//   }
+
+//   return pivot;
+//  }
+// }
